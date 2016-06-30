e58625a98cfdd8bf4a22a15c364735aa99fc4c92
Dmitry Kulagin <kulaginds@gmail.com>
2016-06-30 17:36:27 +0300

    fixed delete url in show.html.erb for hotel; coded show actions

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,24 +1,34 @@
 class HotelsController < ApplicationController
 
 	def index
+		# show list all hotels
+		@hotels = Hotel.all
 	end
 
 	def create
+		# handle new hotel
 	end
 
 	def new
+		# show new hotel form
 	end
 
 	def edit
+		# show edit hotel form
+		@hotel = Hotel.find(params[:id])
 	end
 
 	def show
+		# show hotel
+		@hotel = Hotel.find(params[:id])
 	end
 
 	def update
+		# handle edit hotel
 	end
 
 	def destroy
+		# handle delete hotel
 	end
 
 end
