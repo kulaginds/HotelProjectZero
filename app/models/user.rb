2029875290412397ca7c34a1c03fc4b089433e61
Dmitry Kulagin <kulaginds@gmail.com>
2016-07-04 17:26:05 +0300

    added hotel model validate; fixed hello on home.html.erb; in new and edit hotel page added display errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :username, :email, :password, :password_confirmation
+  # attr_accessor :email, :password, :password_confirmation
 end
