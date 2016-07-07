8f81390dafa38093788ff992ade72253df621615
Dmitry Kulagin <kulaginds@gmail.com>
2016-07-07 17:43:12 +0300

    beautify

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,7 +2,7 @@ class Hotel < ActiveRecord::Base
   mount_uploader :photo, HotelUploader
   validates :title, presence: true
   has_many :comments, dependent: :destroy
-  paginates_per 5
+  paginates_per 6
   ratyrate_rateable "rating"
 
 end
