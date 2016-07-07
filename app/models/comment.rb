fb7ef4f1e8bf3ddd81af33a35147fd51c087bdee
SergeyDzyuba <qpopuk@yandex.ru>
2016-07-07 11:12:18 +0300

    Added comment validation, w/o error messages

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :hotel
+  before_save {commenter.downcase!}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :commenter, presence:true, format: {with: VALID_EMAIL_REGEX}
+  validates :body, presence:true, length: {minimum: 5, maximum: 400}
 end
