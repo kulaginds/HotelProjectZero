2f34fc0ffd5d08f350a2e6df04b9340a21778832
SergeyDzyuba <qpopuk@yandex.ru>
2016-07-08 16:58:47 +0300

    Some fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,5 @@ class Comment < ActiveRecord::Base
   before_save {commenter.downcase!}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :commenter, presence:true, format: {with: VALID_EMAIL_REGEX}
-  validates :body, presence:true, length: {minimum: 5, maximum: 400}
+  validates :body, presence:true, length: {minimum: 5, maximum: 255}
 end
